3116a1d3ebe5f40b2c1611bb9e3d16a928ef8805
Gulam Rasul Shah <99994920+grsLammy@users.noreply.github.com>
2022-12-02 18:15:23 +0530

    feat: refactor code

--- scripts/depositERC721.ts
+++ scripts/depositERC721.ts
@@ -5,11 +5,12 @@ import config from "./utils/config";
 import ps from "prompt-sync";
 const prompt = ps();
 const depositERC721 = async () => {
-    console.log("STARING THE ERC721 TOKEN WITH ITS METADATA BRIDGING PROCESS");
+    console.log("\n-----------------------------------------");
+    console.log("BRIDGE ERC721 TOKEN WITH METADATA PROCESS");
+    console.log("-----------------------------------------\n");
 
     /* ----------------- INPUT ------------------ */
 
-    console.log("\n");
     const tokenID: any = prompt("Enter the tokenID to bridge: ");
     if (!tokenID) return console.log("tokenID cannot be null");
     if (tokenID < 0) return console.log("Invalid tokenID");
@@ -24,35 +25,42 @@ const depositERC721 = async () => {
     let erc721RootToken = await posClient.erc721(config.rootToken, true);
 
     // APPROVE NFT
+    console.log("\n-----------------------------------------");
+    console.log("APPROVE - ERC721 PREDICATE PROXY CONTRACT");
+    console.log("-----------------------------------------\n");
     const approveResponse = await erc721RootToken.approve(tokenID);
-    console.log("\nToken Approved successfully");
     console.log("Approve transaction hash: ", await approveResponse.getTransactionHash());
     console.log(
         `Transaction details: https://goerli.etherscan.io/tx/${await approveResponse.getTransactionHash()}`
     );
+    console.log("\nToken Approved successfully");
 
     // WAIT PERIOD
-    console.log("\nWaiting at least 60secs for the for block confirmation...");
+    console.log("\n-----------------------------------------");
+    console.log("WAITING PERIOD - WAIT FOR AT LEAST 60 SEC");
+    console.log("-----------------------------------------\n");
+    console.log("\nWaiting at least 60 sec...");
     await sleep(60000); // wait at least 1 min for state change in goerli
 
     // ENCODE DATA
     const abiCoder = ethers.utils.defaultAbiCoder;
-    const encodedData = abiCoder.encode(["uint", "address", "string"], [tokenID, config.user, tokenUri]);
+    const depositData = abiCoder.encode(["uint", "address", "string"], [tokenID, config.user, tokenUri]);
 
     // DEPOSIT NFT
-    let depositResponse = await erc721RootToken.deposit(tokenID, config.user, {
-        data: encodedData,
-    });
-    console.log(`\nToken Deposited successfully to ERC721Predicate Contract`);
+    console.log("\n-----------------------------------------");
+    console.log("DEPOSIT - ROOTCHAINMANAGER PROXY CONTRACT");
+    console.log("-----------------------------------------\n");
+    let depositResponse = await erc721RootToken.deposit(tokenID, config.user, { data: depositData });
     console.log("Deposit transaction hash: ", await depositResponse.getTransactionHash());
     console.log(
         `Transaction details: https://goerli.etherscan.io/tx/${await depositResponse.getTransactionHash()}`
     );
+    console.log(`\nToken Deposited successfully to ERC721Predicate Contract`);
 };
 
 depositERC721()
     .then(() => {
-        console.log("\nENDING PROCESS\n");
+        console.log("\n\n---------- ENDING ALL PROCESS ----------\n\n");
         process.exit(0);
     })
     .catch((err) => {
