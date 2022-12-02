ae5f63ced6cf99119eb83180e5ea714ffd38fec3
Gulam Rasul Shah <99994920+grsLammy@users.noreply.github.com>
2022-12-02 16:29:41 +0530

    feat: update config.ts

--- scripts/util/config.ts
+++ scripts/util/config.ts
@@ -1,5 +1,4 @@
 const config = {
-    privatekey: "3aaf25724408ee89faa2d191db1bee758a3a9b3288a6177294f027292fdec3bc",
     user: "0x1c25A817a8b3Aa9349e1484644DAd25b9c4A71AB",
     rootToken: "0x6c031A109B2348a5f396489def7a879722e78ccd",
     childToken: "0xb537a0306d221afAB8821b242282d7C92c14fa6e",
