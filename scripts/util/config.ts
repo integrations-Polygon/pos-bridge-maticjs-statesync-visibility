2f8fad379e5bdba18fcdd1b8833dba9c75ef5ed6
Gulam Rasul Shah <99994920+grsLammy@users.noreply.github.com>
2022-12-02 15:46:51 +0530

    feat: update new root and child token

--- scripts/util/config.ts
+++ scripts/util/config.ts
@@ -1,8 +1,8 @@
 const config = {
     privatekey: "3aaf25724408ee89faa2d191db1bee758a3a9b3288a6177294f027292fdec3bc",
     user: "0x1c25A817a8b3Aa9349e1484644DAd25b9c4A71AB",
-    rootToken: "0x4e3acB13Ab180749645a9206790646AdF4F0FCf0",
-    childToken: "0xD8828c004f6a82C50fB246576A4B7d3D5318E66a",
+    rootToken: "0x6c031A109B2348a5f396489def7a879722e78ccd",
+    childToken: "0xb537a0306d221afAB8821b242282d7C92c14fa6e",
     ERC721PredicateProxy: "0x74D83801586E9D3C4dc45FfCD30B54eA9C88cf9b",
 };
 
