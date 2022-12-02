b56c7d768ac69333443cba4218c455f2745ddd8e
Gulam Rasul Shah <99994920+grsLammy@users.noreply.github.com>
2022-12-02 15:20:12 +0530

    feat: update MyChildNFT contract

--- src/Mumbai/MyChildNFT.sol
+++ src/Mumbai/MyChildNFT.sol
@@ -5,17 +5,22 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import { ContextMixin } from "../utils/ContextMixin.sol";
 import { IChildToken } from "./interface/IChildToken.sol";
 import { AccessControlMixin } from "../utils/AccessControlMixin.sol";
-import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import { NativeMetaTransaction } from "../utils/NativeMetaTransaction.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import { AccessControl } from "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract MyChildNFT is ERC721, ERC721URIStorage, Ownable {
-    address deployer;
-    address childChainManagerProxy;
+contract MyChildNFT is IChildToken, AccessControlMixin, NativeMetaTransaction, ContextMixin, ERC721URIStorage, Ownable {
+    bytes32 public constant DEPOSITOR_ROLE = keccak256("DEPOSITOR_ROLE");
 
-    constructor(address _childChainManagerProxy) ERC721("MyChildNFT", "McN") {
-        childChainManagerProxy = _childChainManagerProxy;
-        deployer = msg.sender;
+    // limit batching of tokens due to gas limit restrictions
+    uint256 public constant BATCH_LIMIT = 20;
+    event WithdrawnBatch(address indexed user, uint256[] tokenIds);
+
+    constructor(string memory _name, string memory _symbol, address _childChainManager) ERC721(_name, _symbol) {
+        _setupContractId("MyChildNFT");
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+        _setupRole(DEPOSITOR_ROLE, _childChainManager);
+        _initializeEIP712(_name);
 
         // Can't mint here, because minting in child chain smart contract's constructor not allowed
         //
@@ -24,31 +29,82 @@ contract MyChildNFT is ERC721, ERC721URIStorage, Ownable {
         // _mint(msg.sender, 10 ** 27);
     }
 
-    function safeMint(address to, uint256 tokenId, string memory uri) public onlyOwner {
-        _safeMint(to, tokenId);
-        _setTokenURI(tokenId, uri);
+    /**
+     * returns the message sender
+     */
+
+    function supportsInterface(bytes4 interfaceId) public view virtual override(AccessControl, ERC721) returns (bool) {
+        return super.supportsInterface(interfaceId);
     }
 
-    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
-        super._burn(tokenId);
+    // This is to support Native meta transactions
+    // never use msg.sender directly, use _msgSender() instead
+    function _msgSender() internal view override returns (address sender) {
+        return ContextMixin.msgSender();
     }
 
-    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+    function tokenURI(uint256 tokenId) public view override(ERC721URIStorage) returns (string memory) {
         return super.tokenURI(tokenId);
     }
 
-    function deposit(address user, bytes calldata depositData) external {
-        require(msg.sender == childChainManagerProxy, "You're not allowed to deposit");
-        uint256 tokenId;
-        address user_address;
-        string memory uri;
-        (tokenId, user_address, uri) = abi.decode(depositData, (uint256, address, string));
-        _safeMint(user, tokenId);
-        _setTokenURI(tokenId, uri);
+    /**
+     * @notice called when token is deposited on root chain
+     * @dev Should be callable only by ChildChainManager
+     * Should handle deposit by minting the required tokenId for user
+     * Make sure minting is done only by this function
+     * @param user user address for whom deposit is being done
+     * @param depositData abi encoded tokenId
+     */
+    function deposit(address user, bytes calldata depositData) external override only(DEPOSITOR_ROLE) {
+        // deposit single
+        if (depositData.length == 32) {
+            uint256 tokenId;
+            string memory uri;
+            address user_address;
+            (tokenId, user_address, uri) = abi.decode(depositData, (uint256, address, string));
+            _mint(user, tokenId);
+            _setTokenURI(tokenId, uri);
+        }
+        // deposit batch
+        else {
+            uint256[] memory tokenIds;
+            string[] memory uris;
+            address[] memory user_addresses;
+            (tokenIds, user_addresses, uris) = abi.decode(depositData, (uint256[], address[], string[]));
+            uint256 length = tokenIds.length;
+            for (uint256 i; i < length; i++) {
+                _mint(user_addresses[i], tokenIds[i]);
+                _setTokenURI(tokenIds[i], uris[i]);
+            }
+        }
     }
 
+    /**
+     * @notice called when user wants to withdraw token back to root chain
+     * @dev Should burn user's token. This transaction will be verified when exiting on root chain
+     * @param tokenId tokenId to withdraw
+     */
     function withdraw(uint256 tokenId) external {
         require(_msgSender() == ownerOf(tokenId), "ChildERC721: INVALID_TOKEN_OWNER");
         _burn(tokenId);
+    }
+
+    /**
+     * @notice called when user wants to withdraw multiple tokens back to root chain
+     * @dev Should burn user's tokens. This transaction will be verified when exiting on root chain
+     * @param tokenIds tokenId list to withdraw
+     */
+    function withdrawBatch(uint256[] calldata tokenIds) external {
+        uint256 length = tokenIds.length;
+        require(length <= BATCH_LIMIT, "ChildERC721: EXCEEDS_BATCH_LIMIT");
+        for (uint256 i; i < length; i++) {
+            uint256 tokenId = tokenIds[i];
+            require(
+                _msgSender() == ownerOf(tokenId),
+                string(abi.encodePacked("ChildERC721: INVALID_TOKEN_OWNER ", tokenId))
+            );
+            _burn(tokenId);
+        }
+        emit WithdrawnBatch(_msgSender(), tokenIds);
     }
 }
