5ffc418b3c29abea9edf04aaf6e1b63c5984b89c
grsLammy <grspro98@gmail.com>
2023-01-18 13:02:33 +0300

    feat: use axios instead of fetch and use testnet api url

--- utils/fetchAbiData.ts
+++ utils/fetchAbiData.ts
@@ -1,12 +1,14 @@
-import fetch from "node-fetch";
-const explorerApiKey = process.env.ETHEREUM_EXPLORER_API_KEY;
+import axios from "axios";
+const explorerApiKey = process.env.POLYGON_EXPLORER_API_KEY;
 
 // Function to fetch your smart contract ABI data
 async function fetchAbiData(contractAddress: any) {
     try {
-        return await fetch(
-            `https://api-goerli.etherscan.io/api?module=contract&action=getabi&address=${contractAddress}&apikey=${explorerApiKey}`
-        );
+        return (
+            await axios.get(
+                `https://api-testnet.polygonscan.com/api?module=contract&action=getabi&address=${contractAddress}&apikey=${explorerApiKey}`
+            )
+        ).data;
     } catch (error) {
         console.log(`Error in fetchAbiData: ${error}`);
         process.exit(1);
